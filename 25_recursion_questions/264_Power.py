fc351c98a29e5fe5011ce24bc6662d4730bfa59b
AlinaGay <gayfullina.a@gmail.com>
2024-03-12 15:38:22 +0100

    add recursion topic and problems

---
+++ 25_recursion_questions/264_Power.py
@@ -0,0 +1,9 @@
+def power(base, exp):
+  assert int(exp) == exp, "The exponent must be positive integer only!"
+  if exp == 0:
+    return 1
+  else:
+    return base * power(base, exp-1)
+  
+
+print(power(2, 4))
