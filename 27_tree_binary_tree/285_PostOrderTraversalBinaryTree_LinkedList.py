eebeb18fdb533d6766fbc0e4c0f01f3c75c6f580
AlinaGay <gayfullina.a@gmail.com>
2024-03-19 14:48:38 +0100

    add bt

---
+++ 27_tree_binary_tree/285_PostOrderTraversalBinaryTree_LinkedList.py
@@ -0,0 +1,21 @@
+class TreeNode:
+  def __init__(self,data):
+    self.data = data
+    self.leftChild = None
+    self.rightChild = None
+
+newBT = TreeNode("Drinks")  
+leftChild = TreeNode("Hot")
+rightChild = TreeNode("Cold")
+newBT.leftChild = leftChild
+newBT.rightChild = rightChild
+
+def postOrderTraversal(rootNode):
+  if not rootNode:
+    return
+  postOrderTraversal(rootNode.leftChild)
+  postOrderTraversal(rootNode.rightChild) 
+  print(rootNode.data)
+     
+
+postOrderTraversal(newBT)
