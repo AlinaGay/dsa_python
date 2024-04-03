33e33cca36cdaef23f687d650428669f9edbfb60
AlinaGay <gayfullina.a@gmail.com>
2024-04-03 22:30:03 +0200

    add array method

--- 3_array/33_OneDimensionalArrayPractice.py
+++ 3_array/33_OneDimensionalArrayPractice.py
@@ -1,7 +1,14 @@
 from array import *
+
 #5 extend array
 arr_1 = array("i", [1,2,3,4,5,6])
 arr_2 = array('i', [8,9,0])
 arr_1.extend(arr_2)
 
+print(arr_1)
+
+#6 add items from list into array using fromList() method
+print("------------------------------------")
+tempList = [20,30,40]
+arr_1.fromlist(tempList)
 print(arr_1)
