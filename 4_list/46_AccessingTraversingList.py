c7f573697c2a8897e090a587d065d2f43c1050fb
AlinaGay <gayfullina.a@gmail.com>
2024-04-19 23:34:48 +0200

    add list method

--- 4_list/46_AccessingTraversingList.py
+++ 4_list/46_AccessingTraversingList.py
@@ -2,3 +2,15 @@ shoppingList = ['Milk','Cheese','Butter']
 
 print(shoppingList[0])
 print('Bread' in shoppingList)
+
+print("------------------------------------------")
+
+for i in range(len(shoppingList)):
+  shoppingList[i] = shoppingList[i] + "+"
+  print(shoppingList[i])
+
+print("------------------------------------------")
+
+empty = []
+for i in empty:
+  print("I am empty")  
