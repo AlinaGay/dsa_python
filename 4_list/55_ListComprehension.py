cea17cac94cb2131ffdb4bbc6713b3bc73eae590
AlinaGay <gayfullina.a@gmail.com>
2024-04-24 22:04:27 +0200

    add list method

---
+++ 4_list/55_ListComprehension.py
@@ -0,0 +1,9 @@
+#new_list = [new_item for item in list]
+
+prev_list = [1,2,3]
+new_list = [el*2 for el in prev_list]
+print(new_list)
+
+language = 'Python'
+letter = [l for l in language]
+print(letter)
