57f68c116849df9da91e705ccd082e7ccd51d111
AlinaGay <gayfullina.a@gmail.com>
2024-04-26 16:20:28 +0200

    add dict method

---
+++ 8_dictionaries/88_CreateDictionary.py
@@ -0,0 +1,11 @@
+my_dict = dict()
+print(my_dict)
+
+another_dict = {}
+print(another_dict)
+
+eng_sp = dict(one='uno',two='dos',three='tres')
+print(eng_sp)
+
+eng_sp2 = {'one':'uno','two':'dos','three':'tres'}
+print(eng_sp)
