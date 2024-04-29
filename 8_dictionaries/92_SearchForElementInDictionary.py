2179a0f98cd82fa6411273b4e9799d0496836aa6
AlinaGay <gayfullina.a@gmail.com>
2024-04-29 22:04:02 +0200

    add dict method

---
+++ 8_dictionaries/92_SearchForElementInDictionary.py
@@ -0,0 +1,9 @@
+myDict = {'name':'Alina', 'age': 37, 'address': 'Serbia'}
+
+def searchDict(dict, value):
+  for key in dict:
+    if dict[key] == value:
+      return key, value
+  return "The value does not exist"
+
+print(searchDict(myDict, 37))  
