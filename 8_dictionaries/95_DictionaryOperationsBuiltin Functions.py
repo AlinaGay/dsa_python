9b619d47419017d5442c55319216fd07591443b7
AlinaGay <gayfullina.a@gmail.com>
2024-05-08 17:13:19 +0200

    add dictionary methods

---
+++ 8_dictionaries/95_DictionaryOperationsBuiltin Functions.py
@@ -0,0 +1,14 @@
+my_dict = {
+  3:'three',
+  4:'four',
+  5:'five',
+  6:'six',
+  7:'seven'
+}
+
+print(3 in my_dict)
+print('three' in my_dict)
+print('three' in my_dict.values())
+print(len(my_dict))
+print(all(my_dict)) # check all keys
+print(sorted(my_dict)) # return list of sorted keys
