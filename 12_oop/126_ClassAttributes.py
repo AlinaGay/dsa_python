680cdba7a117b97750e62460c3502414b4147f75
AlinaGay <gayfullina.a@gmail.com>
2024-05-19 21:39:22 +0200

    add oop methods

---
+++ 12_oop/126_ClassAttributes.py
@@ -0,0 +1,14 @@
+class StarCookie:
+  pass
+
+star_cookie_1 = StarCookie()
+star_cookie_1.weight = 15
+star_cookie_1.color = 'red'
+print(star_cookie_1.weight)
+print(star_cookie_1.color)
+
+star_cookie_2 = StarCookie()
+star_cookie_2.weight = 20
+star_cookie_2.color = 'orange'
+print(star_cookie_2.weight)
+print(star_cookie_2.color)
