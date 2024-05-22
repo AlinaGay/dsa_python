df14bd3957b535eb7cb8d9a14a3866be81fe1b1c
AlinaGay <gayfullina.a@gmail.com>
2024-05-22 18:42:02 +0200

    add oop methods

--- 12_oop/126_ClassAttributes.py
+++ 12_oop/126_ClassAttributes.py
@@ -1,16 +1,15 @@
 class StarCookie:
-  def __init__(self):
+  def __init__(self, color, weight):
     # initialaze attributes
     print("The cookie is ready")
+    self.color = color
+    self.weight = weight
 
-star_cookie_1 = StarCookie()
-star_cookie_1.weight = 15
-star_cookie_1.color = 'red'
+star_cookie_1 = StarCookie("red", 15)
 print(star_cookie_1.weight)
 print(star_cookie_1.color)
 
-star_cookie_2 = StarCookie()
-star_cookie_2.weight = 20
-star_cookie_2.color = 'orange'
+star_cookie_2 = StarCookie('orange', 20)
+star_cookie_2.weight = 30
 print(star_cookie_2.weight)
 print(star_cookie_2.color) 
