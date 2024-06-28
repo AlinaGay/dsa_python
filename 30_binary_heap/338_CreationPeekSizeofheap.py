dbbb16abe0781684887cab2a54953baba2742478
AlinaGay <gayfullina.a@gmail.com>
2024-06-28 15:43:40 +0200

    add heap methods

--- 30_binary_heap/338_CreationPeekSizeofheap.py
+++ 30_binary_heap/338_CreationPeekSizeofheap.py
@@ -17,6 +17,13 @@ def sizeOfHeap(rootNode):
     return
   else:
     return rootNode.heapSize
+  
+def levelOrderTraversal(rootNode):
+  if not rootNode:
+    return
+  else:
+    for i in range(1, rootNode.heapSize + 1):
+      print(rootNode.customList[i])  
 
 print(peekOfHeap(newBH))
 print(sizeOfHeap(newBH)) 
